3bf6fb6c41c2076be2251d669621dd7e6be7fbae
hdghaII <dr.mistical@gmail.com>
2023-11-08 20:29:58 +0300

    a few sections

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -1,1 +1,13 @@
 "use strict";
+var swiper = new swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
