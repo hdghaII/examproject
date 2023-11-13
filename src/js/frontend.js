380a93055efdd5c521803c2322be7bcfa579540c
hdghaII <dr.mistical@gmail.com>
2023-11-13 13:01:04 +0300

    swiper needs fix

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -1,13 +1,23 @@
 "use strict";
-var swiper = new swiper(".mySwiper", {
+document.addEventListener('DOMContentLoaded', () => {
+  var swiper = new Swiper(".swiper", {
     cssMode: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+          // when window width is >= 320px
+          320: {
+              slidesPerView: 1,
+              spaceBetween: 0,
+              slideToClickedSlide: true,
+          }
+      }
+    });
+});
