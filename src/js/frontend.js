3f749dd69a285fceb0d07fd413556149d0cfbe74
hdghaII <dr.mistical@gmail.com>
2023-11-17 18:47:38 +0300

    finished

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -3,13 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
   var swiper = new Swiper(".swiper", {
     cssMode: true,
     slidesPerView: 1,
-    spaceBetween: 0,
+    spaceBetween: 30,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
       pagination: {
         el: ".swiper-pagination",
+        clickable: true,
       },
       breakpoints: {
           // when window width is >= 320px
@@ -20,4 +21,36 @@ document.addEventListener('DOMContentLoaded', () => {
           }
       }
     });
+});
+$('.header__burger--wrapper').on('click', function() {
+  if( !$('.header__burger').hasClass('active') ) {
+     $('.header__burger').addClass('active');
+     $('html').addClass('no-scroll');
+     $('body').addClass('no-scroll')
+     $('.header__menu').addClass('active');
+  } else {
+    $('.header__burger' ).removeClass('active');
+    $('html').removeClass('no-scroll');
+    $('body').removeClass('no-scroll')
+    $('.header__menu').removeClass('active');
+  }
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var newsletterForm = document.querySelector('.footer__newsletter--form');
+
+  newsletterForm.addEventListener('submit', function(event) {
+      var emailInput = document.querySelector('.footer__newsletter--text');
+      var emailValue = emailInput.value.trim();
+
+      if (!isValidEmail(emailValue)) {
+          alert('Please enter a valid email address.');
+          event.preventDefault(); // Prevent the form submission
+      }
+  });
+
+  function isValidEmail(email) {
+      // Use a regular expression to validate the email format
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return emailRegex.test(email);
+  }
 });
